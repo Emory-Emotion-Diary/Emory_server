2873307902d65e3ef0a03c69baff748c999c79f7
박태수 <taesupark0504@gmail.com>
2025-06-22 23:12:29 +0900

    feat :: emojiDelete API

--- src/main/java/com/example/emory_server/domain/emoji/presentation/EmojiController.java
+++ src/main/java/com/example/emory_server/domain/emoji/presentation/EmojiController.java
@@ -1,6 +1,8 @@
 package com.example.emory_server.domain.emoji.presentation;
 
+import com.example.emory_server.domain.emoji.presentation.dto.request.DeleteEmojiRequest;
 import com.example.emory_server.domain.emoji.presentation.dto.request.EmojiRequest;
+import com.example.emory_server.domain.emoji.service.EmojiDeleteService;
 import com.example.emory_server.domain.emoji.service.EmojiUploadService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +15,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class EmojiController {
     private final EmojiUploadService emojiUploadService;
+    private final EmojiDeleteService emojiDeleteService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void UploadEmoji(@RequestPart(name = "file") MultipartFile file,
                             @RequestPart(name = "request") @Valid EmojiRequest request) {
         emojiUploadService.execute(request, file);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void DeleteEmoji(@RequestBody @Valid DeleteEmojiRequest request) {
+        emojiDeleteService.execute(request);
     }
 }
