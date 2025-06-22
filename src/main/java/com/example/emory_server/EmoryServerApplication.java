e51cebdb89da7e2e235e998bb99416109196919f
박태수 <taesupark0504@gmail.com>
2025-06-22 15:06:16 +0900

    chore :: 잡 설정들

---
+++ src/main/java/com/example/emory_server/EmoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.emory_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmoryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emory_server/EmoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emory_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
