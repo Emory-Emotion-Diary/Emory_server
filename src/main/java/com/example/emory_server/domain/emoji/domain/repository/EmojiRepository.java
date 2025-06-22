a71949d41249493d3cc6651c772726b94940988b
박태수 <taesupark0504@gmail.com>
2025-06-22 21:37:18 +0900

    feat :: findByName 쿼리

--- src/main/java/com/example/emory_server/domain/emoji/domain/repository/EmojiRepository.java
+++ src/main/java/com/example/emory_server/domain/emoji/domain/repository/EmojiRepository.java
@@ -4,6 +4,9 @@ import com.example.emory_server.domain.emoji.domain.Emoji;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmojiRepository extends CrudRepository<Emoji, Long> {
+    Optional<Emoji> findByName(String name);
 }
