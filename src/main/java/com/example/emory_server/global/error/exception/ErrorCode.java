64c6930470d836e988e8c8965a55c4b7045de41d
박태수 <taesupark0504@gmail.com>
2025-06-22 21:35:21 +0900

    feat :: ErrorCode EmojiNotFound

--- src/main/java/com/example/emory_server/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/emory_server/global/error/exception/ErrorCode.java
@@ -31,6 +31,9 @@ public enum ErrorCode {
     FAILED_UPLOAD(1001, "업로드 실패"),
     FAILED_DELETE(1002, "삭제 실패"),
 
+    //emoji
+    EMOJI_NOT_FOUND(404, "이모지 찾지 못함"),
+
     // general
     BAD_REQUEST(400, "프론트 탓"),
     INTERNAL_SERVER_ERROR(500, "서버 탓");
