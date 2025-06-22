f3662b5f86959f2d1ff64b8647b3fa06814a61f9
박태수 <taesupark0504@gmail.com>
2025-06-22 17:45:37 +0900

    feat :: DiaryController

---
+++ src/main/java/com/example/emory_server/domain/diary/presentation/DiaryController.java
@@ -0,0 +1,11 @@
+package com.example.emory_server.domain.diary.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/diary")
+@RequiredArgsConstructor
+public class DiaryController {
+}
