4166aa90217d525d927f0acedb5b484b12b95296
박태수 <taesupark0504@gmail.com>
2025-06-22 17:45:16 +0900

    feat :: DiaryRequest

---
+++ src/main/java/com/example/emory_server/domain/diary/presentation/dto/request/DiaryRequest.java
@@ -0,0 +1,9 @@
+package com.example.emory_server.domain.diary.presentation.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class DiaryRequest {
+    private String emojiName;
+    private String content;
+}
