14bb827c3e37a92a7a4f47982b4d4fe4a2c0ddfb
박태수 <taesupark0504@gmail.com>
2025-06-22 21:38:21 +0900

    feat :: createDiary API

--- src/main/java/com/example/emory_server/domain/diary/presentation/DiaryController.java
+++ src/main/java/com/example/emory_server/domain/diary/presentation/DiaryController.java
@@ -1,11 +1,21 @@
 package com.example.emory_server.domain.diary.presentation;
 
+import com.example.emory_server.domain.diary.presentation.dto.request.DiaryRequest;
+import com.example.emory_server.domain.diary.service.CreateDiaryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/diary")
 @RequiredArgsConstructor
 public class DiaryController {
+    private final CreateDiaryService createDiaryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void CreateDiary(@RequestBody @Valid DiaryRequest request) {
+        createDiaryService.execute(request);
+    }
 }
