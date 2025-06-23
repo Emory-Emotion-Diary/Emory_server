7cfdf5e13bfc5e3c3430fc7aa4cf2e38e218eb5b
박태수 <taesupark0504@gmail.com>
2025-06-23 09:01:08 +0900

    feat :: deleteDiary API

--- src/main/java/com/example/emory_server/domain/diary/presentation/DiaryController.java
+++ src/main/java/com/example/emory_server/domain/diary/presentation/DiaryController.java
@@ -2,6 +2,7 @@ package com.example.emory_server.domain.diary.presentation;
 
 import com.example.emory_server.domain.diary.presentation.dto.request.DiaryRequest;
 import com.example.emory_server.domain.diary.service.CreateDiaryService;
+import com.example.emory_server.domain.diary.service.DeleteDiaryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,10 +13,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class DiaryController {
     private final CreateDiaryService createDiaryService;
+    private final DeleteDiaryService deleteDiaryService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void CreateDiary(@RequestBody @Valid DiaryRequest request) {
         createDiaryService.execute(request);
+    }
+
+    @DeleteMapping("/{diary-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteDiary(@PathVariable("diary-id") Long diaryId) {
+        deleteDiaryService.execute(diaryId);
     }
 }
