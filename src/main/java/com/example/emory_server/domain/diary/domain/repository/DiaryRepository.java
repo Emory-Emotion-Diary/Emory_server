30b5ef93a54fdbdc6cf7a7143705581c71268ae1
박태수 <taesupark0504@gmail.com>
2025-07-02 15:17:34 +0900

    refactor :: query에 between 키워드 추가

--- src/main/java/com/example/emory_server/domain/diary/domain/repository/DiaryRepository.java
+++ src/main/java/com/example/emory_server/domain/diary/domain/repository/DiaryRepository.java
@@ -1,18 +1,15 @@
 package com.example.emory_server.domain.diary.domain.repository;
 
 import com.example.emory_server.domain.diary.domain.Diary;
-import com.example.emory_server.domain.diary.presentation.dto.response.DiaryResponse;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends CrudRepository<Diary, Long> {
-    Optional<DiaryResponse> findByDate(LocalDate date);
 
-    List<Diary> findByUserIdAndDate(Long userId, LocalDate start, LocalDate end);
+    List<Diary> findByUserIdAndDateBetween(Long userId, LocalDate start, LocalDate end);
 }
