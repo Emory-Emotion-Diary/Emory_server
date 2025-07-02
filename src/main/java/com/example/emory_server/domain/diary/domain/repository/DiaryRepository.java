4986fcfb202e0c57ed4d3c6a4eb21868bb60c167
박태수 <taesupark0504@gmail.com>
2025-07-02 14:45:23 +0900

    feat :: diary 조회 쿼리

--- src/main/java/com/example/emory_server/domain/diary/domain/repository/DiaryRepository.java
+++ src/main/java/com/example/emory_server/domain/diary/domain/repository/DiaryRepository.java
@@ -1,9 +1,18 @@
 package com.example.emory_server.domain.diary.domain.repository;
 
 import com.example.emory_server.domain.diary.domain.Diary;
+import com.example.emory_server.domain.diary.presentation.dto.response.DiaryResponse;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DiaryRepository extends CrudRepository<Diary, Long> {
+    Optional<DiaryResponse> findByDate(LocalDate date);
+
+    List<Diary> findByUserIdAndDate(Long userId, LocalDate start, LocalDate end);
 }
