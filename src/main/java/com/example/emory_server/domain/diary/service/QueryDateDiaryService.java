976dc7b6c68b4f78f29d19636a9d20ca4be46a8e
박태수 <taesupark0504@gmail.com>
2025-07-02 14:46:02 +0900

    feat :: diary 날 별로 조회

---
+++ src/main/java/com/example/emory_server/domain/diary/service/QueryDateDiaryService.java
@@ -0,0 +1,33 @@
+package com.example.emory_server.domain.diary.service;
+
+import com.example.emory_server.domain.auth.service.facade.UserFacade;
+import com.example.emory_server.domain.diary.domain.Diary;
+import com.example.emory_server.domain.diary.domain.repository.DiaryRepository;
+import com.example.emory_server.domain.diary.exception.DiaryNotFoundException;
+import com.example.emory_server.domain.diary.presentation.dto.response.DiaryResponse;
+import com.example.emory_server.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class QueryDateDiaryService {
+    private final DiaryRepository diaryRepository;
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public DiaryResponse execute(Long diaryId) {
+        User user = userFacade.currentUser();
+
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
+
+        return DiaryResponse.builder()
+                .content(diary.getContent())
+                .date(diary.getDate())
+                .emojiName(diary.getEmoji().getName())
+                .emojiUrl(diary.getEmoji().getS3Url())
+                .build();
+    }
+}
